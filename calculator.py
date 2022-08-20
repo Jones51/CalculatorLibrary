5942a9e248320f5d810f8ec371633523e6ea9c1c
Jones51 <joaomarcosa51@gmail.com>
2022-08-20 11:23:20 -0300

    Add unit tests for calculator

--- calculator.py
+++ calculator.py
@@ -2,8 +2,10 @@
 Calculator library containing basic functions
 """
 
+
 def adding(num1, num2):
     return num1+num2
 
-def subtracting(num1,num2):
+
+def subtracting(num1, num2):
     return num1-num2
